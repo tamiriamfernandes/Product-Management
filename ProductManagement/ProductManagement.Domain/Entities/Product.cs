5965ba0269802720a8d81798771d12833f3b8a45
Tamiriam Fernandes <tamiriamfernandes@gmail.com>
2023-01-11 23:05:28 -0300

    initial

---
+++ ProductManagement/ProductManagement.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace ProductManagement.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; private set; }
+        public DateTime DateFabrication { get; set; }
+        public DateTime DateValidity { get; set; }
+        public int IdProvider { get; set; }
+        public string DescriptionProvider { get; set; }
+        public string DocumentProvider { get; set; }
+
+        public void ActiveProduct(bool active)
+        {
+            this.Active = active;
+        }
+    }
+}
